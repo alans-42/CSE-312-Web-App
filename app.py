4baca03066d1be275ef9f1ff871f0ec84dbabfff
Alan Soto <alansoto1053@yahoo.com>
2024-03-12 23:23:07 -0400

    Added docker files, login/reg form, navbar, funct
    Website can now run using docker.
    Navbar works which can lead to login/register form.
    Added some functionality for the server

--- app.py
+++ app.py
@@ -1,18 +1,39 @@
-from flask import Flask, render_template, request
+from flask import Flask, send_from_directory, make_response, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
     error = None
-    file = render_template("index.html", error=error)
-    return file
+    file = render_template('index.html', error=error)
+    response = make_response(file)
+    response.headers['X-Content-Type-Options'] = 'nosniff'
+    return response
 
-@app.route('/static/<path:path>')
+@app.route('/static/<path:path>', methods=['GET'])
 def send_css(path):
-    return send_from_directory('static', path)
+    file = send_from_directory('static', path)
+    response = make_response(file)
+    response.headers['X-Content-Type-Options'] = 'nosniff'
+    return response
 
-@app.route('/newFile.html')
-def send_new():
-    error = None
-    return render_template("newFile.html", error=error)
+@app.route('/templates/<path:path>', methods=['GET'])
+def send_templates(path):
+    file =  send_from_directory('templates', path)
+    response = make_response(file)
+    response.headers['X-Content-Type-Options'] = 'nosniff'
+    return response
+
+@app.route('/account', methods=['POST'])
+def check_username():
+    username = request.form["username"]
+    password = request.form["password"]
+
+    print(username, password)
+    return index()
+
+if __name__ == "__main__":
+    host = '0.0.0.0'
+    port = 8080
+
+    app.run(debug=True, host=host, port=port)
